64c024625645ac135c4d20690f9bf6e48fc42fac
Artem Altukhov <altukhov.artem.dev@gmail.com>
2017-05-17 22:18:24 +0300

    First version

---
+++ MrRobot/MrRobot/MrRobot.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrRobot
+{
+    class MrRobot
+    {
+
+        public Direction Direction { get; private set; } // текущее направление взгляда
+
+        public int X { get; private set; } // текущая координата X
+
+        public int Y { get; private set; } // текущая координата Y
+
+        public MrRobot()
+        {
+            Direction = Direction.UP;
+            X = 0;
+            Y = 0;
+        }
+
+        public MrRobot(int x, int y)
+        {
+
+        }
+
+
+        public void TurnLeft()
+        {
+            // повернуться на 90 градусов против часовой стрелки
+            switch(Direction)
+            {
+                case Direction.UP: Direction = Direction.LEFT; break;
+                case Direction.LEFT: Direction = Direction.DOWN; break;
+                case Direction.DOWN: Direction = Direction.RIGHT; break;
+                case Direction.RIGHT: Direction = Direction.UP; break;
+            }
+        }
+
+        public void TurnRight()
+        {
+            // повернуться на 90 градусов по часовой стрелке
+            switch (Direction)
+            {
+                case Direction.UP: Direction = Direction.RIGHT; break;
+                case Direction.RIGHT: Direction = Direction.DOWN; break;
+                case Direction.DOWN: Direction = Direction.LEFT; break;
+                case Direction.LEFT: Direction = Direction.UP; break;
+            }
+        }
+
+        public void StepForward()
+        {
+            // шаг в направлении взгляда
+            // за один шаг робот изменяет одну свою координату на единицу
+            switch (Direction)
+            {
+                case Direction.UP: Y++; break;
+                case Direction.RIGHT: X++; break;
+                case Direction.DOWN: Y--; break;
+                case Direction.LEFT: X--; break;
+            }
+        }
+
+        
+    }
+
+    public enum Direction
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+}
---
+++ MrRobot/MrRobot/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrRobot
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            MrRobot robot = new MrRobot();
+            robot.Move(2, 3);
+            robot.CurrentLocation();
+            robot.CurrentDirection();
+            robot.Move(5, 2);
+            robot.CurrentLocation();
+            robot.CurrentDirection();
+            robot.Move(-5, 0);
+            robot.CurrentLocation();
+            robot.CurrentDirection();
+            robot.Move(-5, 0);
+            robot.CurrentLocation();
+            robot.CurrentDirection();
+        }
+
+        public static void Move(this MrRobot robot, int newX, int newY)
+        {
+            int currentX = robot.X;
+            int currentY = robot.Y;
+            if (newX > currentX)
+            {
+                while (robot.Direction != Direction.RIGHT)
+                {
+                    if (robot.Direction == Direction.UP)
+                        robot.TurnRight();
+                    else if (robot.Direction == Direction.DOWN)
+                        robot.TurnLeft();
+                    else
+                        robot.TurnRight();
+                }
+                for (int i = 0; i < newX - currentX; i++)
+                {
+                    robot.StepForward();
+                }
+            }
+            else if (newX < currentX)
+            {
+                while (robot.Direction != Direction.LEFT)
+                {
+                    if (robot.Direction == Direction.UP)
+                        robot.TurnLeft();
+                    else if (robot.Direction == Direction.DOWN)
+                        robot.TurnRight();
+                    else
+                        robot.TurnLeft();
+                }
+                for (int i = 0; i < currentX - newX; i++)
+                {
+                    robot.StepForward();
+                }
+            }
+            if (newY > currentY)
+            {
+                while (robot.Direction != Direction.UP)
+                {
+                    if (robot.Direction == Direction.LEFT)
+                        robot.TurnRight();
+                    else if (robot.Direction == Direction.RIGHT)
+                        robot.TurnLeft();
+                    else
+                        robot.TurnRight();
+                }
+                for (int i = 0; i < newY - currentY; i++)
+                {
+                    robot.StepForward();
+                }
+            }
+            else if (newY < currentY)
+            {
+                while (robot.Direction != Direction.DOWN)
+                {
+                    if (robot.Direction == Direction.RIGHT)
+                        robot.TurnRight();
+                    else if (robot.Direction == Direction.LEFT)
+                        robot.TurnLeft();
+                    else
+                        robot.TurnLeft();
+                }
+                for (int i = 0; i < currentY - newY; i++)
+                {
+                    robot.StepForward();
+                }
+            }
+        }
+
+        public static void CurrentLocation(this MrRobot robot)
+        {
+            Console.WriteLine("X = {0}\nY = {1}", robot.X, robot.Y);
+        }
+
+        public static void CurrentDirection(this MrRobot robot)
+        {
+            Console.WriteLine("Current Direction: {0}", robot.Direction);
+        }
+    }
+}
